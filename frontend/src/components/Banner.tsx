cd3ddc65c0bc5910e3ea9ea3efc1d331657f4bc5
Nick <web6studio@gmail.com>
2025-03-10 12:20:40 +0100

    Update Banner mobile styles

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -14,13 +14,13 @@ export function Banner() {
 
       <div className="relative h-full max-w-container mx-auto px-6">
         <div className="h-full flex flex-col justify-center">
-          <h1 className="font-display text-banner uppercase text-white mb-4">
+          <h1 className="font-display text-banner uppercase text-white mb-4 md:text-left text-center">
             Roasted coffee
           </h1>
-          <p className="text-coffee-text text-subtitle mb-8">
+          <p className="text-coffee-text text-subtitle mb-8 md:text-left text-center">
             Choose a coffee from below or create your own.
           </p>
-          <div className="flex">
+          <div className="flex md:justify-start justify-center">
             <Button size="lg" onClick={() => setIsModalOpen(true)}>
               Create your own coffee
             </Button>
